e3679393304f6cbb0070c18bf1e731b7088c55ce
marmar-biz <biazar.insurance@gmail.com>
2025-11-06 18:20:07 +0330

    Create page.tsx

---
+++ app/categories/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function CategoriesPage() {
+  return (
+    <main className="container mx-auto p-6">
+      <h1>دسته‌ها</h1>
+      <p>اینجا لیست دسته‌بندی‌هاتو می‌ذاری.</p>
+    </main>
+  );
+}
