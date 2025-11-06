a0a0c890274e2bf6896096b1c1ca421f1c53cd1b
marmar-biz <biazar.insurance@gmail.com>
2025-11-06 18:21:25 +0330

    Create page.tsx

---
+++ app/packages/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function PackagesPage() {
+  return (
+    <main className="container mx-auto p-6">
+      <h1>پکیج‌ها</h1>
+      <p>اینجا محصولات/پکیج‌ها نمایش داده می‌شن.</p>
+    </main>
+  );
+}
