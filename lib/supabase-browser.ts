628c8a3b4edc6b9db5c87ecffbaf2657afac9342
marmar-biz <biazar.insurance@gmail.com>
2025-11-08 12:41:22 +0330

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -4,11 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-    },
-  }
+  { auth: { persistSession: true, autoRefreshToken: true } }
 );
